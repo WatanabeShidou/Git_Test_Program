4cc0ef2873a83c191722cd5a2eeee6b92ab1ce82
しどう 渡辺 <k022g1026@g.neec.ac.jp>
2022-10-21 10:04:16 +0900

    2行目をNew branch Changedに変更

--- Git_Test_Program.cpp
+++ Git_Test_Program.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello Github World!\n";
 
-    std::cout << "New branch Changed";
+    std::cout << "New branch Changed!";
 //    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
 //   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
 //   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
