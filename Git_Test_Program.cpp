ca54db95e0ea9ac2dc06ed508685f5eedcbacfee
WatanabeShidou <115197391+WatanabeShidou@users.noreply.github.com>
2022-12-14 19:49:27 +0900

    リモートリポジトリでコンフリクト変更

--- Git_Test_Program.cpp
+++ Git_Test_Program.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello Github World!\n";
 
-    std::cout << "GitHub Clone changed!\n";
+    std::cout << "Git Clone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
